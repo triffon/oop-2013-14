319607c3310944f179aaa7d99f709b2fdb6e4f4d
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-03-17 16:50:00 +0200

    Примерна програма за работа с динамичната памет.

---
+++ dynamic/main.cpp
@@ -0,0 +1,89 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 17.03.2014
+ *      Author: trifon
+ */
+
+#include <iostream>
+using namespace std;
+
+void test() {
+	int* p = new int;
+	*p = 3;
+	int* q = p;
+	*q = 4;
+	cout << *q << endl;
+	int const* r = p;
+	// !!! *r = 5;
+	cout << *r << endl;
+	// !!! p = q = r = NULL;
+
+	double *x;
+	{{{{x = new double[10];}}}}
+	{ double a[10]; cin >> a[3]; }
+	x[1] = 4;
+	for(int i = 0; i < 10; i++)
+		cin >> x[i];
+}
+
+double* resize(double* a, int& capacity) {
+	int newcapacity = capacity * 2;
+	cerr << "Увеличаваме капацитета от " << capacity << " на " << newcapacity << endl;
+	double * newa = new double[newcapacity];
+	for(int i = 0; i < capacity; i++)
+		newa[i] = a[i];
+	delete[] a;
+	capacity = newcapacity;
+	return newa;
+}
+
+void findDuplicates() {
+	int capacity = 10;
+	double* a = new double[capacity];
+	int count = 0;
+	double x;
+	do {
+		cin >> x;
+		a[count++] = x;
+		if (count == capacity)
+			a = resize(a, capacity);
+	} while(x != 0);
+
+	int i = 0, j = count;
+	while (i < count - 1 && j == count) {
+		j = i + 1;
+		while (j < count && a[i] != a[j])
+			j++;
+		// j == count -> не се повтаря
+		// j < count -> намерили сме повторение
+		i++;
+	}
+	// j < count намерили сме повторение
+	if (j < count)
+		cout << "Има повторение!";
+	else
+		cout << "Няма повторение!";
+	delete[] a;
+}
+
+void transposeMatrix() {
+	int n, m;
+	cin >> m >> n;
+
+	int** a = new int*[m]; // указатели към редовете
+	for(int i = 0; i < m; i++)
+		a[i] = new int[n]; // създаваме всеки ред поотделно!
+
+	for(int i = 0; i < m; i++)
+		for(int j = 0; j < n; j++)
+			cin >> a[i][j];
+}
+
+int main() {
+	// findDuplicates();
+	transposeMatrix();
+	return 0;
+}
+
+
