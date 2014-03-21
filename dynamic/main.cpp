4f1db7b72a8d98fc09132ada0c9590ed5f3016d0
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-03-21 13:00:00 +0200

    Довършихме транспониране на матрица. Пример за memory leak.

--- dynamic/main.cpp
+++ dynamic/main.cpp
@@ -70,7 +70,8 @@ void findDuplicates() {
 void transposeMatrix() {
 	int n, m;
 	cin >> m >> n;
-
+	// m реда
+	// n стълба
 	int** a = new int*[m]; // указатели към редовете
 	for(int i = 0; i < m; i++)
 		a[i] = new int[n]; // създаваме всеки ред поотделно!
@@ -78,11 +79,52 @@ void transposeMatrix() {
 	for(int i = 0; i < m; i++)
 		for(int j = 0; j < n; j++)
 			cin >> a[i][j];
+
+	// n реда
+	// m стълба
+	int** b = new int*[n];
+	for(int i = 0; i < n; i++)
+		b[i] = new int[m];
+
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			b[i][j] = a[j][i];
+
+	for(int i = 0; i < m; i++)
+		delete[] a[i];
+	delete[] a;
+
+	// позволено!!!
+	a = b; // но как да изтрия старата матрица???
+
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m; j++)
+			cout << a[i][j] << ' ';
+		cout << endl;
+	}
+//
+}
+
+void memoryLeak() {
+	const int MAX = 1000000;
+	for(int i = 0; i < MAX; i++) {
+		double *a = new double[MAX];
+		for(int j = 0; j < MAX; j++)
+			a[j] = 0;
+		cout << "Step #" << i << endl;
+		cin.get();
+
+		delete[] a;
+	}
+	cout << "Done!" << endl;
+	cin.get();
+
 }
 
 int main() {
 	// findDuplicates();
-	transposeMatrix();
+	// transposeMatrix();
+	memoryLeak();
 	return 0;
 }
 
