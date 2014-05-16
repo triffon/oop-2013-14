e694a2a7d2910f60899266370431b7395a7aa1c9
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2014-05-16 15:14:09 +0300

    Клас PaidStudent с голяма четворка

---
+++ inheritance/paid_student.cpp
@@ -0,0 +1,75 @@
+/*
+ * paid_student.cpp
+ *
+ *  Created on: 16.05.2014
+ *      Author: trifon
+ */
+
+#include "paid_student.h"
+#include <cstring>
+
+PaidStudent::PaidStudent(char const* _name,
+		                 char const* _id,
+		                 int _fn,
+		                 double _grade,
+		                 double _fee,
+		                 char const* _sponsor)
+ : Student(_name, _id, _fn, _grade),
+   fee(_fee), sponsor(NULL) {
+	cerr << "PaidStudent(...)" << endl;
+	setSponsor(_sponsor);
+}
+
+PaidStudent::PaidStudent(PaidStudent const& ps)
+  : Student(ps), sponsor(NULL) {
+	cerr << "PaidStudent(PaidStudent const&)" << endl;
+	copy(ps);
+}
+
+PaidStudent& PaidStudent::operator=(PaidStudent const& ps) {
+	cerr << "operator=(PaidStudent const&)" << endl;
+	if (this != &ps) {
+		// Student::operator=(ps);
+		//(Student &)*this = ps;
+		*(Student*)this = ps;
+		clean();
+		copy(ps);
+	}
+	return *this;
+}
+
+PaidStudent::~PaidStudent() {
+	cerr << "~PaidStudent()" << endl;
+	clean();
+}
+
+void PaidStudent::print(ostream& os) const {
+	Student::print(os);
+	os << "Такса: " << fee << endl;
+	os << "Спонсор: " << sponsor << endl;
+}
+
+void PaidStudent::setFee(double _fee) {
+	fee = _fee;
+}
+
+void PaidStudent::setSponsor(char const* _sponsor) {
+	if (sponsor != NULL)
+		delete[] sponsor;
+	sponsor = new char[strlen(_sponsor) + 1];
+	strcpy(sponsor, _sponsor);
+}
+
+void PaidStudent::copy(PaidStudent const& ps) {
+	// !!! Person::copy(ps);
+	setFee(ps.fee);
+	setSponsor(ps.sponsor);
+}
+
+void PaidStudent::clean() {
+	if (sponsor != NULL)
+		delete[] sponsor;
+}
+
+
+
---
+++ inheritance/paid_student.h
@@ -0,0 +1,44 @@
+/*
+ * paid_student.h
+ *
+ *  Created on: 16.05.2014
+ *      Author: trifon
+ */
+
+#ifndef PAID_STUDENT_H_
+#define PAID_STUDENT_H_
+
+#include <iostream>
+using namespace std;
+#include "student.h"
+
+class PaidStudent : public Student {
+	double fee;
+	char* sponsor;
+public:
+
+	// голяма четворка
+	PaidStudent(char const* ="", char const* ="0",
+				int=0, double=0, double=0, char const* ="аз");
+	PaidStudent(PaidStudent const&);
+	PaidStudent& operator=(PaidStudent const&);
+	~PaidStudent();
+
+	// селектори
+	double getFee() const { return fee; }
+	char const* getSponsor() const { return sponsor; }
+	void print(ostream& = cout) const;
+
+	// мутатори
+	void setFee(double);
+	void setSponsor(char const*);
+
+private:
+	// копиране и почистване
+	void copy(PaidStudent const&);
+	void clean();
+};
+
+
+
+#endif /* PAID_STUDENT_H_ */
